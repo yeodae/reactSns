3ab3b9a6930a00c0827ab5388f7f532e86f7cea1
yeodae <130977379+yeodae@users.noreply.github.com>
2023-09-19 10:40:34 +0900

    최종 마무리

--- src/app/join/page.js
+++ src/app/join/page.js
@@ -52,12 +52,12 @@ export default function Join() {
         <div className='loginBox'>
           <h2 className='h2'>𝑰𝒏𝒔𝒅𝒐𝒈𝒓𝒂𝒎</h2>
           <div className='loginBox2'>
-            <h3 className='stadyInfo'>기본정보</h3>
+            <h3 className='stadyInfo'>회원가입</h3>
             <div><input type='text' name="email" placeholder='이메일 주소'></input></div>
             <div><input type='text' name="name" placeholder='성명'></input></div>
             <div><input type='text' name="name2" placeholder='사용자 이름'></input></div>
             <div><input type='password' name="pw" placeholder='비밀번호'></input></div>
-            <div><input type="submit" value="가입"></input></div>
+            <div><input type="submit" value="회원가입"></input></div>
           </div>
           <div className='loginGo'>
             계정이 있으신가요? <a href='/login'>로그인</a>
